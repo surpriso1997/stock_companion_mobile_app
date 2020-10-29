a9c7f45b56ecc08c67a4c396ad3da2c30c9afe8b
manish <maneesh10chaulagain@gmail.com>
2020-10-29 18:32:02 +0545

    made static ui of dashboard page

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -8,8 +9,136 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("dashboard"),
+    _buildGridViewItem({String title, IconData icon, Function onPressed}) {
+      return Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).textSelectionColor,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 1, color: Theme.of(context).primaryColor),
+            boxShadow: [
+              BoxShadow(
+                color: Theme.of(context).primaryColor,
+                offset: Offset(3, 3),
+                spreadRadius: 0.5,
+                blurRadius: 2.0,
+              )
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 35, color: Theme.of(context).primaryColor),
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).primaryColor,
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return SafeArea(
+      top: true,
+      child: GridView(
+        // itemCount: 10,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 0.93,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
+        // itemBuilder: (context, index) {
+        //   return Container(
+        //       child: Column(
+        //     children: [Icon(Icons.home), Text("My Portfolio")],
+        //   ));
+        // },
+
+        children: [
+          _buildGridViewItem(
+              icon: Icons.business_center,
+              title: 'My Portfolio',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: MaterialCommunityIcons.timer_sand,
+              title: 'Live Nepse',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: AntDesign.barchart, title: 'Top Trades', onPressed: () {}),
+          _buildGridViewItem(
+              icon: Entypo.line_graph, title: 'Stock Price', onPressed: () {}),
+          _buildGridViewItem(
+              icon: Icons.analytics, title: 'Floorsheet', onPressed: () {}),
+          _buildGridViewItem(
+              icon: AntDesign.piechart,
+              title: 'Market Indices',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: Feather.target, title: 'Market Depth', onPressed: () {}),
+          _buildGridViewItem(
+              icon: AntDesign.eyeo, title: 'Watchlist', onPressed: () {}),
+          _buildGridViewItem(
+              icon: Icons.notification_important_outlined,
+              title: 'Stock Alert',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: SimpleLineIcons.notebook, title: 'Notes', onPressed: () {}),
+          _buildGridViewItem(
+              icon: MaterialIcons.next_week,
+              title: 'Proposed',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: MaterialCommunityIcons.door,
+              title: 'Openings',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: Feather.globe, title: 'SEBON Issues', onPressed: () {}),
+          _buildGridViewItem(
+              icon: Icons.forum, title: 'Forum', onPressed: () {}),
+          _buildGridViewItem(
+              icon: Icons.new_releases, title: 'News', onPressed: () {}),
+          _buildGridViewItem(
+              icon: FontAwesome.mortar_board,
+              title: 'Stock Education',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: AntDesign.notification, title: 'Notices', onPressed: () {}),
+          _buildGridViewItem(
+              icon: FontAwesome.handshake_o,
+              title: 'Mero Share',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: MaterialCommunityIcons.ring,
+              title: 'Gold Silver',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: Feather.dollar_sign,
+              title: 'Exchange Rates',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: FontAwesome.institution,
+              title: 'Listed Stocks',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: MaterialCommunityIcons.office_building,
+              title: 'Listed Brokers',
+              onPressed: () {}),
+          _buildGridViewItem(
+              icon: Icons.calculate, title: 'Calculator', onPressed: () {}),
+        ],
+      ),
     );
   }
+}
+
+class DashboardItem {
+  final String title;
+  final IconData icon;
+  final String routeName;
+  const DashboardItem({this.title, this.icon, this.routeName});
 }
