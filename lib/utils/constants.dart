ece72106d060649f2db9ccdd0ec3bf413faa97f5
manish <maneesh10chaulagain@gmail.com>
2020-10-30 18:58:34 +0545

    added eror messages constants  for api calls

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,4 +4,14 @@ class Constants {
 
   static const base_url_prod = 'newweb.nepalstock.com/';
   static const secondary_base_url_prod = '';
+
+  /// error message
+  static const error_msg_404 = "Something is wrong";
+  static const error_msg_500 = "Server could not process the request.";
+  static const error_msg_401 = "Usser is not authnticated";
+  static const error_msg_408 = "Connection Timeout occured!";
+  static const error_msg_timeout = "Connection Timeout occured!";
+  static const error_mdg_422 = "Could not process your request at the moment";
+  static const error_error = "An error occured!";
+  static const unauthorized_error = "User is unauthorized";
 }
