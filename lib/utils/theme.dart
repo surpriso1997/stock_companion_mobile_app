d835592db50a0b46dbabcbcae5d442cf8c41efdb
manish <maneesh10chaulagain@gmail.com>
2020-10-30 18:59:13 +0545

    added more entires in theme

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -2,28 +2,32 @@ import 'package:flutter/material.dart';
 
 enum AppTheme { Dark, Light, NightBlue, AmoledBlack, DarkGrey }
 
-final primay_color = Color(0xff253559);
+final primay_color = Color(0xff0D023B);
 final primary_color_dark = Color(0xff253559);
 final primary_text_highlight_color = Color(0xffF6B129);
 final font_family = "Roboto";
 
 final _baseTheme = ThemeData(
-    primaryColor: primay_color,
-    buttonTheme: ButtonThemeData(),
-    primaryColorDark: primary_color_dark,
-    textSelectionColor: primary_text_highlight_color,
-    fontFamily: font_family,
-    scaffoldBackgroundColor: Colors.black87,
-    textTheme: TextTheme(),
-    bottomAppBarColor: Colors.black87,
-    bottomNavigationBarTheme:
-        BottomNavigationBarThemeData(backgroundColor: Colors.black87));
+  primaryColor: primay_color,
+  backgroundColor: Colors.white,
+  buttonTheme: ButtonThemeData(),
+  primaryColorDark: primary_color_dark,
+  textSelectionColor: primary_text_highlight_color,
+  fontFamily: font_family,
+  scaffoldBackgroundColor: Colors.black87,
+  textTheme: TextTheme(),
+  bottomAppBarColor: Colors.black87,
+  bottomNavigationBarTheme:
+      BottomNavigationBarThemeData(backgroundColor: Colors.black87),
+);
 
 final themeData = {
-  AppTheme.Dark: _baseTheme.copyWith(scaffoldBackgroundColor: Colors.black87),
+  AppTheme.Dark: _baseTheme.copyWith(
+      scaffoldBackgroundColor: Colors.black87, cardTheme: CardTheme()),
   AppTheme.Light: _baseTheme.copyWith(
-    primaryColorDark: primary_color_dark,
-    textSelectionColor: primary_text_highlight_color,
-    scaffoldBackgroundColor: Colors.white,
-  )
+      primaryColorDark: primary_color_dark,
+      backgroundColor: Colors.black,
+      textSelectionColor: primary_text_highlight_color,
+      scaffoldBackgroundColor: Colors.white,
+      cardTheme: CardTheme())
 };
