51d127146138a8b0a5d7adc2f52f21997450322a
manish <maneesh10chaulagain@gmail.com>
2020-11-03 20:48:37 +0545

    add market repository and bloc in app and bloc lists

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stock_companion/bloc/theme_cubit/theme_cubit.dart';
+import 'package:stock_companion/data/repository/market_repository.dart';
 
 import 'package:stock_companion/services/services.dart';
 import 'package:stock_companion/utils/app_config.dart';
@@ -14,24 +12,25 @@ import 'utils/utils.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocRepositories(
-      child: MultiBlocProviders(
-        child: BlocBuilder<ThemeCubit, ThemeState>(
-          builder: (context, state) {
-            // data: state.theme,
-            // isMaterialAppTheme: Platform.isAndroid,
-            return MaterialApp(
-              // builder: DevicePreview.appBuilder,
-              title: AppConfig.of(context).appName,
-              theme: state.theme,
-              darkTheme: themeData[AppTheme.Dark],
-              home: Homepage(),
-              debugShowCheckedModeBanner: AppConfig.of(context).flavor == "dev",
-              routes: Routes.routes,
-              navigatorKey: Navigation.navigationKey,
-            );
-          },
-        ),
+    var appconfig = AppConfig.of(context);
+
+    return MultiBlocProviders(
+      marketReposity: IMarketReposity(baseUrl: appconfig.baseUrl),
+      child: BlocBuilder<ThemeCubit, ThemeState>(
+        builder: (context, state) {
+          // data: state.theme,
+          // isMaterialAppTheme: Platform.isAndroid,
+          return MaterialApp(
+            // builder: DevicePreview.appBuilder,
+            title: AppConfig.of(context).appName,
+            theme: state.theme,
+            darkTheme: themeData[AppTheme.Dark],
+            home: Homepage(),
+            debugShowCheckedModeBanner: AppConfig.of(context).flavor == "dev",
+            routes: Routes.routes,
+            navigatorKey: Navigation.navigationKey,
+          );
+        },
       ),
     );
   }
--- lib/utils/bloc_lists.dart
+++ lib/utils/bloc_lists.dart
@@ -1,28 +1,32 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:flutter/material.dart';
+import 'package:stock_companion/bloc/market_summary/market_summary_bloc.dart';
 import 'package:stock_companion/bloc/theme_cubit/theme_cubit.dart';
-import 'package:stock_companion/data/repository/market_summary_repository.dart';
+import 'package:stock_companion/data/repository/market_repository.dart';
 
 class MultiBlocProviders extends StatelessWidget {
   final Widget child;
-  const MultiBlocProviders({this.child});
+  MultiBlocProviders({this.child, @required IMarketReposity marketReposity})
+      : _iMarketReposity = marketReposity;
+
+  final IMarketReposity _iMarketReposity;
+
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => ThemeCubit())],
-      child: child,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(value: _iMarketReposity),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => ThemeCubit()),
+          BlocProvider(
+              create: (context) =>
+                  MarketSummaryCubit(marketrepo: _iMarketReposity))
+        ],
+        child: child,
+      ),
     );
-  }
-}
-
-class MultiBlocRepositories extends StatelessWidget {
-  final Widget child;
-  const MultiBlocRepositories({this.child});
-  @override
-  Widget build(Object context) {
-    return MultiRepositoryProvider(providers: [
-      RepositoryProvider(create: (context) => MarketRepository())
-    ], child: child);
   }
 }
