d96bc0ca8f4d2c443585517a3ee53ea6f1e14932
manish <maneesh10chaulagain@gmail.com>
2020-11-17 13:59:06 +0545

    create boilerplate page for company

---
+++ lib/pages/company.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class Company extends StatefulWidget {
+  @override
+  _CompanyState createState() => _CompanyState();
+}
+
+class _CompanyState extends State<Company> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
