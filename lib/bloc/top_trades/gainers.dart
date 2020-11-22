ce4a5ca2d88541acc3c671656cc4fa4e5c9add40
manish <maneesh10chaulagain@gmail.com>
2020-11-22 17:07:52 +0545

    used blocbuilder to  create gainers page

--- lib/bloc/top_trades/gainers.dart
+++ lib/bloc/top_trades/gainers.dart
@@ -4,14 +4,50 @@ import 'package:bloc/bloc.dart';
 import 'package:stock_companion/bloc/bloc/common_event.dart';
 import 'package:stock_companion/bloc/bloc/common_state.dart';
 import 'package:stock_companion/data/repository/top_trades.dart';
+import 'package:stock_companion/models/models.dart';
+import 'package:stock_companion/utils/custom_exceptions.dart';
 
 class GainersBloc extends Bloc<CommonEvent, CommonState> {
   GainersBloc({ITopTradesRepo repository})
       : _repo = repository,
         super(FetchingItemsState());
   final ITopTradesRepo _repo;
+
+  List<TopItem> _gainers = [];
   @override
   Stream<CommonState> mapEventToState(
     CommonEvent event,
-  ) async* {}
+  ) async* {
+    if (event is FetchItems) {
+      if (_repo.gainers.isNotEmpty) {
+        yield FetchedItemsState(items: _repo.gainers);
+      } else
+        yield FetchingItemsState();
+
+      try {
+        var res =
+            await _repo.getTopGainers(isRefreshRequest: event.isRefreshRequest);
+
+        if (res.isEmpty) {
+          yield NoDataState();
+        } else if (res.isNotEmpty) {
+          // _gainers.addAll(res);
+
+          yield FetchedItemsState(items: res);
+        }
+      } on ApiException catch (e) {
+        yield ErrorState(message: e.message);
+      }
+    } else if (state is RefreshErrorState) {
+      yield RefreshingItems(items: _repo.gainers);
+      try {
+        var res = await _repo.getTopGainers();
+        if (res.isNotEmpty) {
+          yield FetchedItemsState(items: res);
+        }
+      } on ApiException catch (e) {
+        yield RefreshErrorState(message: e.message, items: _repo.gainers);
+      }
+    }
+  }
 }
--- lib/pages/top_trades/widgets/gainers.dart
+++ lib/pages/top_trades/widgets/gainers.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:stock_companion/bloc/bloc/common_event.dart';
 import 'package:stock_companion/bloc/bloc/common_state.dart';
 import 'package:stock_companion/bloc/top_trades/gainers.dart';
+import 'package:stock_companion/models/top_trades.dart';
+import 'package:stock_companion/pages/top_trades/widgets/common.dart';
+import 'package:stock_companion/utils/utils.dart';
+import 'package:stock_companion/widgets/fucntional_widgets.dart';
 
 class Gainers extends StatefulWidget {
   @override
@@ -13,17 +19,53 @@ class _GainersState extends State<Gainers> {
   @override
   void initState() {
     super.initState();
+
+    BlocProvider.of<GainersBloc>(context).add(FetchItems());
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<GainersBloc, CommonState>(builder: (context, state) {
       if (state is FetchingItemsState) {
+        return progressIndicator();
       } else if (state is ErrorState) {
+        return errorWidget(state.message, () {});
       } else if (state is NoDataState) {
-      } else if (state is RefreshingItems || state is FetchedItemsState) {
-      } else
-        return Container();
+        return noDataFound();
+      } else if (state is RefreshingItems || state is FetchedItemsState) {}
+      return Column(
+        children: [
+          Flexible(
+            child: SingleChildScrollView(
+                child: DataTable(
+                    headingRowColor: MaterialStateProperty.resolveWith(
+                        (states) => Colors.green),
+                    headingTextStyle: Theme.of(context)
+                        .dataTableTheme
+                        .headingTextStyle
+                        .copyWith(color: whiteC),
+                    columns: [
+                      DataColumn(label: Text('SN')),
+                      DataColumn(label: Text('LTP')),
+                      DataColumn(label: Text('Change')),
+                    ],
+                    rows: List.generate(state.listItems.length, (index) {
+                      TopItem _item = state.listItems[index];
+                      final _style = TextStyle(color: blackC, fontSize: 16);
+                      return DataRow(cells: [
+                        DataCell(Text(
+                          _item.symbol,
+                          style: _style,
+                        )),
+                        DataCell(Text(_item.ltp.toString(), style: _style)),
+                        DataCell(Text(
+                            "${_item.pointChange} (${_item.percentageChange} %)",
+                            style: _style.copyWith(fontWeight: bold)))
+                      ]);
+                    }))),
+          ),
+        ],
+      );
     });
   }
 }
