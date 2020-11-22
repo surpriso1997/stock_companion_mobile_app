f006ecb852b39bc2c604066a9ef8c61da5e2a8a6
manish <maneesh10chaulagain@gmail.com>
2020-11-22 12:31:58 +0545

    listed newly created top traderd blocs and setup DI for them

--- lib/utils/bloc_lists.dart
+++ lib/utils/bloc_lists.dart
@@ -3,14 +3,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:stock_companion/bloc/market_summary/market_summary_bloc.dart';
 import 'package:stock_companion/bloc/theme_cubit/theme_cubit.dart';
+import 'package:stock_companion/bloc/top_trades/top_trades_blocs.dart';
 import 'package:stock_companion/data/repository/market_repository.dart';
+import 'package:stock_companion/data/repository/top_trades.dart';
 
 class MultiBlocProviders extends StatelessWidget {
   final Widget child;
-  MultiBlocProviders({this.child, @required IMarketReposity marketReposity})
-      : _iMarketReposity = marketReposity;
+  MultiBlocProviders({
+    this.child,
+    @required IMarketReposity marketReposity,
+    @required ITopTradesRepo topTradedRepository,
+  })  : _iMarketReposity = marketReposity,
+        _iTopTradesRepo = topTradedRepository;
 
   final IMarketReposity _iMarketReposity;
+  final ITopTradesRepo _iTopTradesRepo;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +27,20 @@ class MultiBlocProviders extends StatelessWidget {
       ],
       child: MultiBlocProvider(
         providers: [
-          BlocProvider(create: (context) => ThemeCubit()),
+          BlocProvider(
+              create: (context) => BrokersBloc(repository: _iTopTradesRepo)),
+          BlocProvider(
+              create: (context) => LosersBloc(repository: _iTopTradesRepo)),
+          BlocProvider(
+              create: (context) =>
+                  TransactionsBloc(repository: _iTopTradesRepo)),
+          BlocProvider(
+              create: (context) =>
+                  SharesTradedBloc(repository: _iTopTradesRepo)),
+          BlocProvider(
+              create: (context) => TurnoverBloc(repository: _iTopTradesRepo)),
+          BlocProvider(
+              create: (context) => BrokersBloc(repository: _iTopTradesRepo)),
           BlocProvider(
               create: (context) =>
                   MarketSummaryCubit(marketrepo: _iMarketReposity))
