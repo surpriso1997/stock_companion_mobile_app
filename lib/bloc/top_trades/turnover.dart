725627a34d8345b9eb8c98c2a187a58ef3ea0328
manish <maneesh10chaulagain@gmail.com>
2020-11-22 17:08:46 +0545

    used bloc builder to create turnover page

--- lib/bloc/top_trades/turnover.dart
+++ lib/bloc/top_trades/turnover.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:stock_companion/bloc/bloc/common_event.dart';
 import 'package:stock_companion/bloc/bloc/common_state.dart';
 import 'package:stock_companion/data/repository/top_trades.dart';
+import 'package:stock_companion/utils/utils.dart';
 
 class TurnoverBloc extends Bloc<CommonEvent, CommonState> {
   TurnoverBloc({ITopTradesRepo repository})
@@ -13,5 +14,35 @@ class TurnoverBloc extends Bloc<CommonEvent, CommonState> {
   @override
   Stream<CommonState> mapEventToState(
     CommonEvent event,
-  ) async* {}
+  ) async* {
+    if (event is FetchItems) {
+      if (_repo.turnovers.isNotEmpty) {
+        yield FetchedItemsState(items: _repo.turnovers);
+      } else
+        yield FetchingItemsState();
+
+      try {
+        var res = await _repo.getTopTurnovers(
+            isRefreshRequest: event.isRefreshRequest);
+
+        if (res.isEmpty) {
+          yield NoDataState();
+        } else if (res.isNotEmpty) {
+          yield FetchedItemsState(items: res);
+        }
+      } on ApiException catch (e) {
+        yield ErrorState(message: e.message);
+      }
+    } else if (state is RefreshErrorState) {
+      yield RefreshingItems(items: _repo.turnovers);
+      try {
+        var res = await _repo.getTopTurnovers();
+        if (res.isNotEmpty) {
+          yield FetchedItemsState(items: res);
+        }
+      } on ApiException catch (e) {
+        yield RefreshErrorState(message: e.message, items: _repo.turnovers);
+      }
+    }
+  }
 }
--- lib/pages/top_trades/widgets/turnover.dart
+++ lib/pages/top_trades/widgets/turnover.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stock_companion/bloc/bloc/common_event.dart';
+import 'package:stock_companion/bloc/bloc/common_state.dart';
+import 'package:stock_companion/bloc/top_trades/top_trades_blocs.dart';
+import 'package:stock_companion/models/models.dart';
+import 'package:stock_companion/utils/utils.dart';
+import 'package:stock_companion/widgets/fucntional_widgets.dart';
 
 class Turnover extends StatefulWidget {
   @override
@@ -7,8 +14,64 @@ class Turnover extends StatefulWidget {
 }
 
 class _TurnoverState extends State<Turnover> {
+  getTurnover(turn) {
+    var _indouble = double.tryParse(turn);
+    if (_indouble != null) {
+      _indouble = _indouble / 10000000;
+    }
+    return _indouble.toStringAsFixed(4);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<TurnoverBloc>(context).add(FetchItems());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final _theme = Theme.of(context);
+    return BlocBuilder<TurnoverBloc, CommonState>(builder: (context, state) {
+      if (state is FetchingItemsState) {
+        return progressIndicator();
+      } else if (state is ErrorState) {
+        return errorWidget(state.message, () {});
+      } else if (state is NoDataState) {
+        return noDataFound();
+      } else if (state is RefreshingItems || state is FetchedItemsState) {}
+      return Column(
+        children: [
+          Flexible(
+            child: SingleChildScrollView(
+                child: DataTable(
+                    // horizontalMargin: 0.1,
+
+                    headingTextStyle: Theme.of(context)
+                        .dataTableTheme
+                        .headingTextStyle
+                        .copyWith(color: whiteC),
+                    columns: [
+                      DataColumn(label: Text('SY')),
+                      DataColumn(label: Text('Turnover(Cr.)')),
+                      DataColumn(label: Text('LTP')),
+                    ],
+                    rows: List.generate(state.listItems.length, (index) {
+                      TopItem _item = state.listItems[index];
+                      final _style = _theme.dataTableTheme.dataTextStyle;
+                      return DataRow(cells: [
+                        DataCell(Text(
+                          _item.symbol,
+                          style: _style.copyWith(fontWeight: FontWeight.bold),
+                        )),
+                        DataCell(Text(getTurnover(_item.turnover.toString()),
+                            style: _style)),
+                        DataCell(
+                            Text(_item.closingPrice.toString(), style: _style)),
+                      ]);
+                    }))),
+          ),
+        ],
+      );
+    });
   }
 }
