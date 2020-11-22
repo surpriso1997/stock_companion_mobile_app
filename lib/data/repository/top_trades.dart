54e7f84ea703038a1348833368dc760e62649264
manish <maneesh10chaulagain@gmail.com>
2020-11-22 17:09:32 +0545

    added methods  and varibales for gainers, losers and turnover in top trades repository

--- lib/data/repository/top_trades.dart
+++ lib/data/repository/top_trades.dart
@@ -8,8 +8,18 @@ class ITopTradesRepo {
 
   ITopTradesRepo({String baseUrl}) : _baseUrl = baseUrl;
 
-  Future<List<TopItem>> getTopGainers({bool isRefreshRequest = false}) async {
-    final url = "$_baseUrl/";
+  List<TopItem> _gainers = [];
+  List<TopItem> get gainers => _gainers;
+
+  List<TopItem> _losers = [];
+  List<TopItem> get losers => _losers;
+
+  List<TopItem> _turnovers = [];
+  List<TopItem> get turnovers => _turnovers;
+
+  Future<List<TopItem>> getTopGainers(
+      {bool isRefreshRequest = false, int size = 10}) async {
+    final url = "$_baseUrl/nots/top-ten/top-gainer";
 
     var time = DateTime.now();
     if (time.hour > 15 || time.hour < 11) {
@@ -19,19 +29,66 @@ class ITopTradesRepo {
     try {
       final res = (await getRequest(
         url: url,
-        params: {},
+        params: {'all': true, 'size': size},
       )) as List;
 
       var topItems = res.map<TopItem>((e) => TopItem.fromJson(e)).toList();
-
-      return topItems;
+      _gainers.clear();
+      _gainers.addAll(topItems);
+      return _gainers;
     } catch (e) {
-      throw ApiException(message: e?.nessage ?? Constants.error_error);
+      throw ApiException(message: e?.message ?? Constants.error_error);
     }
   }
 
-  getTopLosers() async {}
-  getTurnovers() async {}
+  Future<List<TopItem>> getTopLosers(
+      {bool isRefreshRequest = false, int size = 10}) async {
+    final url = "$_baseUrl/nots/top-ten/top-loser";
+
+    var time = DateTime.now();
+    if (time.hour > 15 || time.hour < 11) {
+      isRefreshRequest = false;
+    }
+
+    try {
+      final res = (await getRequest(
+        url: url,
+        params: {'all': true, 'size': size},
+      )) as List;
+
+      var topItems = res.map<TopItem>((e) => TopItem.fromJson(e)).toList();
+      _losers.clear();
+      _losers.addAll(topItems);
+      return _losers;
+    } catch (e) {
+      throw ApiException(message: e?.message ?? Constants.error_error);
+    }
+  }
+
+  Future<List<TopItem>> getTopTurnovers(
+      {bool isRefreshRequest = false, int size = 10}) async {
+    final url = "$_baseUrl/nots/top-ten/turnover";
+
+    var time = DateTime.now();
+    if (time.hour > 15 || time.hour < 11) {
+      isRefreshRequest = false;
+    }
+
+    try {
+      final res = (await getRequest(
+        url: url,
+        params: {'all': true, 'size': size},
+      )) as List;
+
+      var topItems = res.map<TopItem>((e) => TopItem.fromJson(e)).toList();
+      _turnovers.clear();
+      _turnovers.addAll(topItems);
+      return _turnovers;
+    } catch (e) {
+      throw ApiException(message: e?.message ?? Constants.error_error);
+    }
+  }
+
   getSharesTraded() async {}
   getTransactions() async {}
   getBrokers() async {}
