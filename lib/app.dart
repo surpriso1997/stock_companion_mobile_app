63aefc5de057a7a470f252b3929ccb46cd4f3e29
manish <maneesh10chaulagain@gmail.com>
2021-01-29 21:09:17 +0545

    [add] list company repository in app

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stock_companion/bloc/theme_cubit/theme_cubit.dart';
+import 'package:stock_companion/data/repository/company_repo.dart';
 import 'package:stock_companion/data/repository/floorsheet_repo.dart';
 import 'package:stock_companion/data/repository/market_repository.dart';
 import 'package:stock_companion/data/repository/top_trades.dart';
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       marketReposity: IMarketReposity(baseUrl: appconfig.baseUrl),
       topTradedRepository: ITopTradesRepo(baseUrl: appconfig.baseUrl),
       floorSheetRepository: IFloorSheetRepo(baseUrl: appconfig.baseUrl),
+      companyRepository: ICompanyRepo(baseUrl: appconfig.baseUrl),
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, state) {
           // data: state.theme,
