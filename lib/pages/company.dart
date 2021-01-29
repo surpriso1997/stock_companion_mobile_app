d9b1f51abec321aa3f39b4171a8bb9889c030ef5
manish <maneesh10chaulagain@gmail.com>
2021-01-29 21:10:27 +0545

    [create] create minimal ui and link with bloc for company list page

--- lib/pages/company.dart
+++ lib/pages/company.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stock_companion/bloc/bloc/common_event.dart';
+import 'package:stock_companion/bloc/bloc/common_state.dart';
+import 'package:stock_companion/bloc/company/company_bloc.dart';
+import 'package:stock_companion/models/company_list_model.dart';
+import 'package:stock_companion/pages/company_details.dart';
+import 'package:stock_companion/utils/utils.dart';
+import 'package:stock_companion/widgets/fucntional_widgets.dart';
 
 class Company extends StatefulWidget {
   @override
@@ -8,7 +16,58 @@ class Company extends StatefulWidget {
 
 class _CompanyState extends State<Company> {
   @override
+  void initState() {
+    super.initState();
+
+    BlocProvider.of<CompanyBloc>(context).add(FetchItems());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listed Companies"),
+      ),
+      body: Container(
+        child: BlocBuilder<CompanyBloc, CommonState>(
+          builder: (context, state) {
+            if (state is FetchingItemsState) {
+              return progressIndicator();
+            } else if (state is ErrorState) {
+              return errorWidget(state.message, () {});
+            } else if (state is NoDataState) {
+              return noDataFound();
+            } else if (state is RefreshingItems || state is FetchedItemsState) {
+              List<CompanyListModel> _list = state.listItems;
+              return ListView.builder(
+                itemCount: _list.length,
+                itemBuilder: (context, index) {
+                  var _item = _list[index];
+                  return ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => CompanyDetails(
+                                    company: _item,
+                                  )));
+                    },
+                    title: Text(
+                      _item.companyName,
+                      style: TextStyle(color: blackC),
+                    ),
+                    leading: Text(
+                      _item.symbol,
+                      style: TextStyle(color: blackC),
+                    ),
+                    subtitle: Text(_item.sectorName),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
