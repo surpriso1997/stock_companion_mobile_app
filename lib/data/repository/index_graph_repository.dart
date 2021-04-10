d848f53e32a23c512b6629db460ae721c6a3e043
manish <maneesh10chaulagain@gmail.com>
2021-04-10 13:49:00 +0545

    chnages made to add nepse's index graph again

--- lib/data/repository/index_graph_repository.dart
+++ lib/data/repository/index_graph_repository.dart
@@ -1,31 +1,86 @@
+import 'package:flutter/foundation.dart';
 import 'package:stock_companion/data/provider/api.dart';
+import 'package:stock_companion/utils/utils.dart';
 
 class IIndexGraphRepository {
   IIndexGraphRepository({String baseUrl}) : _baseUrl = baseUrl;
 
   final String _baseUrl;
 
-  List _chartData = [];
-  List get chartData => _chartData;
-  IndexData _indexData;
-  IndexData get indexData => _indexData;
+  List<List<num>> _chartData = [];
+  List<List<num>> get chartData => _chartData;
 
-  getIndexChartData({var duration, int indexId = 58}) async {
-    var url = _baseUrl + "graph/index/$indexId";
+  var _indexData;
+  get indexData => _indexData;
+
+  getIndexChartData({
+    var duration,
+    String indexType = Constants.NEPSE,
+    int from,
+    int to,
+    int indexId = 58,
+  }) async {
+    var url = _baseUrl + "/nots/graph/index/$indexId";
+
+    if (from == null) {
+      from =
+          DateTime.now().subtract(Duration(days: 365)).millisecondsSinceEpoch;
+      from = (from / 1000).floor();
+    }
+    if (to == null) {
+      to = DateTime.now().millisecondsSinceEpoch;
+      to = (to / 1000).floor();
+    }
+
+    // var url =
+    //     "https://nepsealpha.com/trading/1/history?symbol=$indexData&resoluion=1D&from=$from&to=$to";
+
+    var params = {
+      "symbol": indexType,
+      "resolution": "1D",
+      "from": (from / 1000).floor(),
+      "to": (to / 1000).floor()
+    };
+    print(params);
 
     try {
-      List res = getRequest(url: url);
+      List res = await getRequest(
+        url: url,
+        // params: params,
+      );
+      print(res.toString());
 
       if (res.isNotEmpty) {
-        var data = IndexData.line(res);
-        // _chartData = data;
+        // IndexData _tempData = IndexData.fromJson(res);
 
-        _indexData = data;
-        return res;
-        // return data;
+        ///TODO: for nepse alpha data
+        // for (int i = 0; i < _tempData.time.length; i++) {
+        //   List<num> _data = [
+        //     _tempData.time[i],
+        //     _tempData.open[i],
+        //     _tempData.close[i],
+        //     _tempData.low[i],
+        //     _tempData.high[i],
+        //     _tempData.value[i],
+        //   ];
+        //   data.add(_data);
+        // }
+        //
+
+        _chartData = res.map<List<num>>((e) {
+          List<num> _a = [];
+
+          _a.add(e[0]);
+          _a.add(e[1]);
+
+          _chartData.add(_a);
+          return _a;
+        }).toList();
+
+        return _chartData;
       }
     } catch (e) {
-      return [];
+      throw ApiException(message: e?.toString());
     }
   }
 
@@ -42,14 +97,46 @@ class TimeStampIndex {
 }
 
 class IndexData {
-  final List<TimeStampIndex> data;
+  final List<num> time;
+  final List<num> open;
+  final List<num> high;
+  final List<num> low;
+  final List<num> close;
+  final List<num> value;
 
-  const IndexData({this.data});
+  const IndexData(
+      {this.time, this.open, this.high, this.low, this.close, this.value});
 
-  factory IndexData.line(List<List<dynamic>> list) {
+  factory IndexData.fromJson(Map json) {
     return IndexData(
-        data: list
-            .map<TimeStampIndex>((e) => TimeStampIndex.fromList(e))
-            .toList());
+        close: json['c'],
+        open: json['o'],
+        high: json['h'],
+        low: json['l'],
+        value: json['v']);
   }
+}
+
+class LineGraphData {
+  final num open;
+  final num close;
+  final num high;
+  final num low;
+  final num value;
+  final num time;
+
+  const LineGraphData(
+      {@required this.open,
+      @required this.close,
+      @required this.low,
+      @required this.high,
+      @required this.value,
+      @required this.time});
+}
+
+class NepseLineGraphData {
+  final num value;
+  final num time;
+
+  const NepseLineGraphData({this.value, this.time});
 }
--- lib/widgets/index_graph.dart
+++ lib/widgets/index_graph.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stock_companion/bloc/index_graph/index_graph_bloc.dart';
+import 'package:stock_companion/data/repository/index_graph_repository.dart';
 import 'package:stock_companion/pages/pages.dart';
 import 'package:stock_companion/utils/data_filter.dart';
 import 'package:stock_companion/utils/index_chart_data.dart';
+import 'package:stock_companion/widgets/fucntional_widgets.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class IndexGraph extends StatefulWidget {
@@ -16,38 +18,52 @@ class _IndexGraphState extends State<IndexGraph> {
   Widget build(BuildContext context) {
     return BlocBuilder<IndexGraphBloc, IndexGraphState>(
         builder: (context, state) {
-      return SfCartesianChart(
-          onZooming: (args) {},
-          onZoomStart: (args) {},
-          onZoomReset: (args) {},
-          onZoomEnd: (args) {},
-          zoomPanBehavior: ZoomPanBehavior(
-            enableDoubleTapZooming: false,
-            maximumZoomLevel: 1,
-            enablePanning: true,
-            enablePinching: true,
-          ),
-          trackballBehavior: TrackballBehavior(
-              shouldAlwaysShow: true,
-              enable: true,
-              markerSettings: TrackballMarkerSettings(color: Colors.pink),
-              activationMode: ActivationMode.singleTap,
-              lineType: TrackballLineType.vertical),
-          primaryXAxis: CategoryAxis(),
-          series: <LineSeries<IndexData, dynamic>>[
-            LineSeries<IndexData, dynamic>(
-                enableTooltip: true,
-                dataSource: DataFilter.filterData(index_data, [])
-                    .map<IndexData>(
-                        (e) => IndexData(indexValue: e[1], timestamp: e[0]))
-                    .toList(),
-                yValueMapper: (a, yValue) => a.indexValue,
-                xValueMapper: (datum, int index) {
-                  return DataFilter.parseDate(datum.timestamp).toString();
-                },
-                xAxisName: "Time",
-                yAxisName: "Nepse"),
-          ]);
+      if (state is FetchingGraph || state is IndexGraphInitial) {
+        return progressIndicator();
+      } else if (state is FetchedGraph) {
+        List<List<num>> data = state.data;
+
+        return SfCartesianChart(
+            onZooming: (args) {},
+            onZoomStart: (args) {},
+            onZoomReset: (args) {},
+            onZoomEnd: (args) {},
+            zoomPanBehavior: ZoomPanBehavior(
+              enableDoubleTapZooming: false,
+              maximumZoomLevel: 1,
+              enablePanning: true,
+              enablePinching: true,
+            ),
+            trackballBehavior: TrackballBehavior(
+                shouldAlwaysShow: true,
+                enable: true,
+                markerSettings: TrackballMarkerSettings(color: Colors.pink),
+                activationMode: ActivationMode.singleTap,
+                lineType: TrackballLineType.vertical),
+            primaryXAxis: CategoryAxis(),
+            series: <LineSeries<NepseLineGraphData, dynamic>>[
+              LineSeries<NepseLineGraphData, dynamic>(
+                  enableTooltip: true,
+                  dataSource: DataFilter.filterData(state.data, [])
+                      .map<NepseLineGraphData>((e) => NepseLineGraphData(
+                            // open: e.open,
+                            // close: e.close,
+                            // low: e.low,
+                            // high: e.high,
+                            value: e[1],
+                            time: e[0],
+                          ))
+                      .toList(),
+                  yValueMapper: (a, yValue) => a.value,
+                  xValueMapper: (datum, int index) {
+                    return DataFilter.parseDate(datum.time).toString();
+                  },
+                  xAxisName: "Time",
+                  yAxisName: "Nepse"),
+            ]);
+      } else {
+        return Container();
+      }
     });
   }
 }
