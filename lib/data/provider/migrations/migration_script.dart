56e540405673c3f1acbfc6306a9629889a6717aa
manish <maneesh10chaulagain@gmail.com>
2021-04-14 15:04:42 +0545

    update migration scripts

--- lib/data/provider/migrations/migration_script.dart
+++ lib/data/provider/migrations/migration_script.dart
@@ -2,7 +2,9 @@ final migrationScripts = {
   1: _create_user_table,
   2: _create_wishlist_table,
   3: _create_portfolio_table,
-  4: _create_transactions_table
+  4: _create_transactions_table,
+  5: _create_porfolio_stocks,
+  6: _create_stock_table
 };
 
 const user_table = 'user';
@@ -10,13 +12,15 @@ const wishlist_table = "whishlist";
 const portfolio_table = "portfolio";
 const stocks_table = "stocks";
 
+const portfolio_stocks = "portfolio_stocks";
+
 const transactions_table = "transcations";
 
 final _create_user_table = '''CREATE TABLE "$user_table"(
   id INTEGER,
   full_name varchar(100),
   gender VARCHAR,
-  mobile_number NUMERIC,
+  mobileNumber NUMERIC,
   country VARCHAR,
   address VARCHAR
 )
@@ -30,41 +34,55 @@ alert INTEGER
 ''';
 
 final _create_portfolio_table = '''CREATE TABLE "$portfolio_table"(
- portfolio_id INTEGER,
-portfolio_name VARCHAR,
-porfolio_type VARCHAR
+portfolioId INTEGER,
+portfolioName VARCHAR,
+portfolioType VARCHAR
 )
 ''';
 
-final _create_transactions_table = ''''
+final _create_transactions_table = '''
 CREATE TABLE "$transactions_table" (
-  portfolio_id INTEGER,
-  company_id INTEGER,
-  stock_buy_type VARCHAR,
-  transcation_type VARCHAR
-  no_of_units INTEGER,
-  transaction_price NUMERIC,
-  transaction_date VARCHAR,
-  last_transaction_price   NUMERIC,
-  last_closing_price NUMERIC,
-  todays_change NUMERIC
-
+  portfolioId INTEGER,
+  comapnyId INTEGER,
+  stockBuyType VARCHAR,
+  transactionType VARCHAR
+  noOfUnits INTEGER,
+  transactionPrice NUMERIC,
+  transactionDate VARCHAR,
+  lastTransactionPrice   NUMERIC,
+  lastClosingPrice NUMERIC,
+  todaysChange NUMERIC
 )
-
 ''';
 
 // transaction_type =buy.sell
 // stock_buy_type= ipo/fpo/secondary
 // portfolio_type =individual/insittutional
 
-final _create_stock_table = ''''
-CREATE TABLE "$wishlist_table"(
-  company_id INTEGER,
+final _create_stock_table = '''
+CREATE TABLE "$stocks_table"(
+  companyId INTEGER,
+  name VARCHAR,
   symbol VARCHAR,
-  company_category VARCHAR,
-  company_category_id INTEGER,
-  ltp VARCHAR,
+  sectorName VARCHAR,
+  sectorId INTEGER,
+  lastUpdatedPrice VARCHAR,
   change NUMERIC,
-  change_percent DECIMAL
+
+  changePercent DECIMAL
+
+)
+''';
+
+final _create_porfolio_stocks = '''
+CREATE TABLE "$portfolio_stocks"(
+  companyId INTEGER,
+  symbol VARCHAR,
+  sectorName VARCHAR,
+  sectorId INTEGER,
+  lastTradedPrice VARCHAR,
+  buyingPrice varchar,
+  buyingUnits varchar,
+  buyingDate varchar
 )
 ''';
