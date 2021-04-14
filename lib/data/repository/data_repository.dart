b294a99bd195fa3b63df304b5bcbc88c4270be23
manish <maneesh10chaulagain@gmail.com>
2021-04-15 04:07:30 +0545

    create func to get companies data

--- lib/data/repository/data_repository.dart
+++ lib/data/repository/data_repository.dart
@@ -1,4 +1,5 @@
-import 'package:stock_companion/models/stock_price.dart';
+import 'package:stock_companion/data/provider/api.dart';
+import 'package:stock_companion/models/company_dailyData.dart';
 
 class DataRepository {
   static DataRepository _instance = DataRepository._private();
@@ -7,10 +8,27 @@ class DataRepository {
 
   factory DataRepository() => _instance;
 
-  List<StockPrice> _companiesList = [];
-  List<StockPrice> get companies => _companiesList;
+  List<CompanyDataModel> _companiesList = [];
+  List<CompanyDataModel> get companies => _companiesList;
 
-  setCompaniesList(List<StockPrice> items) {
+  setCompaniesList(List<CompanyDataModel> items) {
     _companiesList = items;
+  }
+
+  getDataOfAllCompanies() async {
+    var url =
+        "https://newweb.nepalstock.com/api/nots/securityDailyTradeStat/58";
+
+    try {
+      List res = await getRequest(url: url);
+
+      var data = res
+          .map<CompanyDataModel>((e) => CompanyDataModel.fromJson(e))
+          .toList();
+
+      _companiesList = data;
+    } catch (e) {
+      print(e);
+    }
   }
 }
