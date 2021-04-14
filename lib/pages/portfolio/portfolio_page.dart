fe0565fefcb06a30dec790dd06a0961e1d38af52
manish <maneesh10chaulagain@gmail.com>
2021-04-14 15:06:21 +0545

    create page for portfolio

---
+++ lib/pages/portfolio/portfolio_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Portfolio extends StatefulWidget {
+  @override
+  _PortfolioState createState() => _PortfolioState();
+}
+
+class _PortfolioState extends State<Portfolio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
